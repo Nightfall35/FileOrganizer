8b5214537a2c7db68a768b1c191fb5d690667615
Ishmael Tembo <162961287+Nightfall35@users.noreply.github.com>
2025-06-25 09:53:29 -0700

    Update DirectoryItem.java

--- src/main/java/org/learn/model/DirectoryItem.java
+++ src/main/java/org/learn/model/DirectoryItem.java
@@ -1,14 +1,17 @@
 package org.learn.model;
 
+import java.nio.file.Path;
+import java.util.Objects;
+
 public class DirectoryItem implements Item{
     private final String name;
     private final String path;
     private final String extension;
 
     public DirectoryItem(String name, String path, String extension) {
-        this.name = name;
-        this.path = path;
-        this.extension = extension;
+        this.name = Objects.requireNonNull(name,"Name cannot be null");
+        this.path = Objects.requireNonNull(path,"Path cannot be null");
+        this.extension = extension != null ? extension :"";
     }
 
     @Override
@@ -24,5 +27,28 @@ public class DirectoryItem implements Item{
     @Override
     public String getExtension() {
         return extension;
+    }
+
+    @Override
+    public String toString() {
+        return "DirectoryItem{" +
+                "name="+path +'\''+
+                ", path="+path +
+                ",extension='"+ extension +'\''+
+                '}';
+    }
+
+    @Override 
+    public boolean equals(Object o) {
+        if(this == 0) return true;
+        if(!(o instanceof DirectoryItem that)) return false;
+        return name.equals(that.name) &&
+               path.equals(that.path) &&
+               extension.equals(that.extension);
+    }
+
+    @Override 
+    public int hashCode() {
+        return Objects.hash(name, path, extension);
     }
 }
